d9874e4dec01feb3578bcf6431264d47af08bcc5
songmoro <wlqgkr@naver.com>
2025-07-25 14:33:39 +0900

    Initial Commit

---
+++ UIKitPractice21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPractice21
+//
+//  Created by 송재훈 on 7/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
