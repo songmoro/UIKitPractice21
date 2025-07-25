2d7fb67d56e462436b5c4e704d583d395b731b7c
songmoro <wlqgkr@naver.com>
2025-07-25 16:48:25 +0900

    rename | networking

---
+++ UIKitPractice21/View Controller/ShopViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ShopViewController.swift
+//  UIKitPractice21
+//
+//  Created by 송재훈 on 7/25/25.
+//
+
+import UIKit
+import Alamofire
+import SnapKit
+
+final class ShopViewController: BaseViewController {
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        searchBar.searchBarStyle = .minimal
+        
+        return searchBar
+    }()
+    
+    override internal func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+}
+
+extension ShopViewController {
+    private func configure() {
+        configureNavigation()
+        configureSubview()
+        configureLayout()
+        configureSearchBar()
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "쇼핑"
+    }
+    
+    private func configureSubview() {
+        view.addSubviews(searchBar)
+    }
+    
+    private func configureLayout() {
+        searchBar.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.top.equalToSuperview(\.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension ShopViewController: UISearchBarDelegate {
+    private func configureSearchBar() {
+        searchBar.delegate = self
+    }
+    
+    internal func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        search(searchBar.text)
+    }
+    
+    private func search(_ text: String?) {
+        print(#function)
+        do {
+            guard let text else { throw ShopViewControllerErrorReason.textIsNil }
+            guard text.count >= 2 else { throw ShopViewControllerErrorReason.textIsLowerThanTwo }
+            
+            try call(text)
+            
+        }
+        catch(let error) {
+            print(error)
+        }
+    }
+    
+    private func call(_ text: String) throws {
+        let url = URL(string: "https://openapi.naver.com/v1/search/shop.json?query=\(text)")
+        guard let url else { throw ShopViewControllerErrorReason.urlIsInvalid }
+        
+        let headers = HTTPHeaders([
+            "X-Naver-Client-Id": APIKey.naverClientId,
+            "X-Naver-Client-Secret": APIKey.naverClientSecret
+        ])
+        
+        AF.request(url, method: .get, headers: headers)
+            .responseDecodable(of: ShopResponse.self) {
+                print($0)
+            }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+}
+
+struct ShopResponse: Decodable {
+    let total: Int
+    let items: [ShopItem]
+}
+
+struct ShopItem: Decodable {
+    let title: String
+    let image: String
+    let brand: String
+    let lprice: String
+    let hprice: String
+}
+
+fileprivate enum ShopViewControllerErrorReason: Error {
+    case textIsNil
+    case textIsLowerThanTwo
+    case urlIsInvalid
+}
